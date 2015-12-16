db313240feb3a428a76e81f7bdcace6f6c16571a
wl-ak <wl-ak@wonderslab.com>
2015-12-16 15:48:19 +0200

    Working on retailers

--- public/wp-content/themes/Wembley/page-retailers.php
+++ public/wp-content/themes/Wembley/page-retailers.php
@@ -1,1 +1,10 @@
-kgheriu
+<?php
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		</main>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
