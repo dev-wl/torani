56dc6390c4301ccf3951848c244038661e43c667
wl-ak <wl-ak@wonderslab.com>
2015-12-21 18:09:23 +0200

    about

--- public/wp-content/themes/Wembley/page-about.php
+++ public/wp-content/themes/Wembley/page-about.php
@@ -1,77 +1,66 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * The Template for displaying all single posts.
  *
  * @package web2feel
  */
 
 get_header(); ?>
-<?php
-	global $post;
-	setup_postdata( $post );
-?>
 
-<style>
-	.description {
-		display: block !important;
-	}
-
-	article.blogpost {
-		margin-top: 130px;
-	}
-
-	#page {
-		/*padding-bottom: 150px !important;*/
-	}
-
-
-</style>
-<div id="primary" class="content-area">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		
-<article id="post-<?php the_ID(); ?>" <?php post_class('col-md-12 blogpost'); ?> >
+		<?php while ( have_posts() ) : the_post(); ?>
+			
+			<article id="post-<?php the_ID(); ?>" <?php post_class('col-lg-12 col-md-12 col-sm-12 col-xs-12 single-product'); ?>>
 
-		<?php
-			$thumb = get_post_thumbnail_id();
-			$img_url = wp_get_attachment_url( $thumb,'full' ); //get full URL to image (use "large" or "medium" if the images too big)
-			$image = aq_resize( $img_url, 1200, 720, true ); //resize & crop the image
-		?>
-					
-		<?php if($image) : ?>
-			<div class="image-block">
-				<img class="img-responsive singlepic" src="<?php echo $image ?>"/>
+			<?php
+				$thumb = get_post_thumbnail_id();
+				$img_url = wp_get_attachment_url( $thumb,'full' ); //get full URL to image (use "large" or "medium" if the images too big)
+				$image = aq_resize( $img_url, 720, 560, true ); //resize & crop the image
+			?>
+						
+			<?php if($image) : ?>
+				<div class="image-block">
+					<img class="img-responsive" src="<?php echo $image ?>"/>
+					<div class="share-icons">
+						<span class="share">share</span> <?php echo do_shortcode('[huge_it_share]'); //echo do_shortcode('[DISPLAY_ULTIMATE_PLUS]'); ?>
+					</div>
+				</div>
+			<?php endif; ?>	
+
+			<div class="general">
+				<div class="description">
+					<h2 class="box-title"><?php the_title(); ?></h2>
+				</div>
+				<div class="box-meta"><?php the_content(); ?></div>	
 				<div class="share-icons">
-					<span class="share">share</span> <?php echo do_shortcode('[huge_it_share]'); //do_shortcode('[DISPLAY_ULTIMATE_PLUS]'); ?>
+					<span class="share">share</span> <?php echo do_shortcode('[huge_it_share]'); //echo do_shortcode('[DISPLAY_ULTIMATE_PLUS]'); ?>
+				</div>
+				<div class="options">
+					<a href="<?php echo get_post_meta($post->ID, 'buy-now-link', true); ?>" class="torani-btn buy-now" target="_blank">BUY NOW</a>
 				</div>
 			</div>
-		<?php endif; ?>
 
-	<div class="general">
-		<div class="description">
-			<h2 class="page-title"><?php the_title(); ?></h2>
-		</div><!-- header -->
-		<div class="box-meta"><?php the_content(); ?></div>	
-		<div class="share-icons">
-			<span class="share">share</span> <?php echo do_shortcode('[huge_it_share]'); //do_shortcode('[DISPLAY_ULTIMATE_PLUS]'); ?>
-		</div>
-	</div><!-- .entry-content -->
-<div class="clearfix"></div>
-</article><!-- #post-## -->
-</main><!-- #main -->
+			<div class="clearfix"></div>
+	</article><!-- #post-## -->
+
+	<div class="col-md-12">
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				// if ( comments_open() || '0' != get_comments_number() )
+					// comments_template();
+			?>
+			</div>
+		<?php endwhile; // end of the loop. ?>
+
+		
+		</main><!-- #main -->
 	</div><!-- #primary -->
 <script>
-	$(document).ready(function() {
-		setTimeout(function() {
-			$(".sfsiplus_inerCnt").find("div.sfsi_tool_tip_2").hide();
-			// $(".sfsiplus_inerCnt").find("div.sfsi_tool_tip_2").show();
-			// alert('done');
-		}, 500);
-	});
+	// $('#main.site-main .recipe img').appendTo($('.post_images'));
+	// $('#page').addClass('single-details');
+	$('.post .share-icons:eq(0)').width('100%'); //$('.img-responsive').width());
 </script>
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
