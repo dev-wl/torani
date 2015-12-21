fa7117211f58a2f6e374a7721229cb31a6d62751
wl-ak <wl-ak@wonderslab.com>
2015-12-21 17:45:24 +0200

    centering iph main

--- public/wp-content/themes/Wembley/page-about.php
+++ public/wp-content/themes/Wembley/page-about.php
@@ -38,14 +38,14 @@ get_header(); ?>
 			$thumb = get_post_thumbnail_id();
 			$img_url = wp_get_attachment_url( $thumb,'full' ); //get full URL to image (use "large" or "medium" if the images too big)
 			$image = aq_resize( $img_url, 1200, 720, true ); //resize & crop the image
-		?>
-					
-		<?php if($image) : ?>
-			<div class="image-block">
-				<img class="img-responsive singlepic" src="<?php echo $image ?>"/> <div class="clearfix"></div>
-				<div class="share-icons">
+		?><div class="clearfix"></div>
+					<div class="clearfix"></div>
+		<?php if($image) : ?><div class="clearfix"></div>
+			<div class="image-block"><div class="clearfix"></div>
+				<img class="img-responsive singlepic" src="<?php echo $image ?>"/>
+				<div class="share-icons"><div class="clearfix"></div>
 					<span class="share">share</span> <?php echo do_shortcode('[huge_it_share]'); //do_shortcode('[DISPLAY_ULTIMATE_PLUS]'); ?>
-				</div>
+				</div><div class="clearfix"></div>
 			</div>
 		<?php endif; ?>	
 
