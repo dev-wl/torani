c30a8d85cf6b4503decc9f71765878840838fa95
wl-ak <wl-ak@wonderslab.com>
2015-12-22 13:34:06 +0200

    Fixing product share links

--- public/wp-content/themes/Wembley/single-product.php
+++ public/wp-content/themes/Wembley/single-product.php
@@ -71,6 +71,13 @@ get_header(); ?>
 			old_medial_link = pinterest.attr('href').substring(pinterest.attr('href').indexOf('&media'), pinterest.attr('href').lastIndexOf('&description'));
 			pinterest.attr('href', pinterest.attr('href').replace(old_medial_link, "&media=" + $('.image-block img').attr('src') ));
 		}
+
+		if($('.box-meta img:eq(0)').find('img').length > 0) {
+			img = $('.box-meta img:eq(0)');
+			pinterest = $('.box-meta .share-icons .huge-it-share-buttons-list a:eq(2)');
+			old_medial_link = pinterest.attr('href').substring(pinterest.attr('href').indexOf('&media'), pinterest.attr('href').lastIndexOf('&description'));
+			pinterest.attr('href', pinterest.attr('href').replace(old_medial_link, "&media=" + $(img).attr('src') ));
+		}
 	});
 	
 </script>
