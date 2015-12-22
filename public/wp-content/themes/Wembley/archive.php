8e341f41baf65b02ee2d17a05adefad1f2be36c4
wl-ak <wl-ak@wonderslab.com>
2015-12-22 11:54:32 +0200

    Fixing product share links

--- public/wp-content/themes/Wembley/archive.php
+++ public/wp-content/themes/Wembley/archive.php
@@ -125,7 +125,7 @@ get_header(); ?>
 <script type="text/javascript">
 	$(document).ready(function() {
 		alert('a');
-		$.each($('.flex-main article', function() {
+		$.each($('.flex-main article'), function() {
 			//get direct product link 
 			prod_link = $(this).find('a.read-more').attr('href');
 			//clear it
