93805c73907cefab332479fd756fbe0796e1b46d
wl-ak <wl-ak@wonderslab.com>
2015-12-22 16:24:31 +0200

    Fixing product share links

--- public/wp-content/themes/Wembley/index.php
+++ public/wp-content/themes/Wembley/index.php
@@ -31,6 +31,7 @@ get_header(); ?>
 	
 	<div id="primary" class="content-area ">
 		<main id="main" class="site-main" role="main">
+		ukbhiuibiuybuyuyuybybiuibui
 			<div class="description">
 				<h1 class="page-title">BLOG</h1> 
 				<?php 
