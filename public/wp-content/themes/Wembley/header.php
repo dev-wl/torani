743f72ee51a002070a2c7632d0725aec5ad145f6
Patrick Jones <patrickjones@Patricks-MacBook-Pro.local>
2016-01-25 08:49:57 -0500

    Added Justuno script

--- public/wp-content/themes/Wembley/header.php
+++ public/wp-content/themes/Wembley/header.php
@@ -254,7 +254,9 @@
 
 
 </script>
-
+<!--Justuno Script-->
+<script type="text/javascript" charset="utf-8">var ju_num="3836E8A2-80FE-4473-BD03-A2A18EB8E0E3";var asset_host=(("https:"==document.location.protocol)?"https":"http")+'://d2j3qa5nc37287.cloudfront.net/';(function() {var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=asset_host+'coupon_code1.js';var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);})();</script>
+<!--End Justuno-->
 </head>
 
 <body <?php body_class(); ?>>
